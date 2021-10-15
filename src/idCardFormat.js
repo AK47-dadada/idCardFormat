c63d8587a103626d9a08f80fce64fd0419f706af
premier <premier@premierfurnitureco.com>
2021-10-15 11:04:32 +0800

    性别bug处理

--- src/idCardFormat.js
+++ src/idCardFormat.js
@@ -24,40 +24,42 @@ class IdCardFormat {
    * 获取指定的身份证编码片段
    * @returns {String}
    */
-  getSpecifiedCode(specified) {
-    return this.idCardNo.slice(...specified)
+  #getSpecifiedCode(specified, idCard) {
+    const idCardNo = idCard || this.idCardNo
+    // 如果校验不通过则返回空
+    return this.idCardValidator(idCardNo) ? idCardNo.slice(...specified) : null
   }
 
   /**
    * 获取省份编码
    * @returns {String}
    */
-  getProvinceCode() {
-    return this.getSpecifiedCode([0, 2])
+  getProvinceCode(idCard) {
+    return this.#getSpecifiedCode([0, 2], idCard)
   }
 
   /**
    * 获取市编码
    * @returns {String}
    */
-  getCityCode() {
-    return this.getSpecifiedCode([2, 4])
+  getCityCode(idCard) {
+    return this.#getSpecifiedCode([2, 4], idCard)
   }
 
   /**
     * 获取区编码
     * @returns {String}
    */
-  getCountyCode() {
-    return this.getSpecifiedCode([4, 6])
+  getCountyCode(idCard) {
+    return this.#getSpecifiedCode([4, 6], idCard)
   }
 
   /**
    * 获取省份名称以及省份code
    * @returns {Object}
    */
-  getProvince() {
-    const proviceCode = `${this.getProvinceCode()}0000`
+  getProvince(idCard) {
+    const proviceCode = `${this.getProvinceCode(idCard)}0000`
     return {
       province: pcaa['86'][proviceCode],
       proviceCode: proviceCode
@@ -68,11 +70,11 @@ class IdCardFormat {
     * 获取市以及市code
     * @returns {Object}
    */
-  getCity() {
-    const { proviceCode } = this.getProvince()
-    const cityCode = `${this.getProvinceCode()}${this.getCityCode()}00`
+  getCity(idCard) {
+    const { proviceCode } = this.getProvince(idCard)
+    const cityCode = `${this.getProvinceCode(idCard)}${this.getCityCode(idCard)}00`
     return {
-      ...this.getProvince(),
+      ...this.getProvince(idCard),
       city: pcaa[proviceCode][cityCode],
       cityCode
     }
@@ -82,14 +84,14 @@ class IdCardFormat {
    * 获取区以及区code
    * @returns
    */
-  getCounty() {
-    const provice = this.getProvinceCode()
-    const city = this.getCityCode()
+  getCounty(idCard) {
+    const provice = this.getProvinceCode(idCard)
+    const city = this.getCityCode(idCard)
     const cityCode = `${provice}${city}00`
-    const countyCode = `${provice}${city}${this.getCountyCode()}`
+    const countyCode = `${provice}${city}${this.getCountyCode(idCard)}`
     return {
-      ...this.getProvince(),
-      ...this.getCity(),
+      ...this.getProvince(idCard),
+      ...this.getCity(idCard),
       county: pcaa[cityCode][countyCode],
       countyCode
     }
@@ -99,8 +101,8 @@ class IdCardFormat {
    * 查询生日
    * @returns {Object}
    */
-  getBirthday() {
-    const birthday = this.getSpecifiedCode([6, 14])
+  getBirthday(idCard) {
+    const birthday = this.#getSpecifiedCode([6, 14])
     return {
       birthday: birthday.replace(/(.{4})(.{2})/, '$1-$2-')
     }
@@ -110,16 +112,15 @@ class IdCardFormat {
    * 获取年龄
    * @returns {Object}
    */
-  getAge() {
-    const birthday = this.getSpecifiedCode([6, 14])
+  getAge(idCard) {
+    const birthday = this.#getSpecifiedCode([6, 14], idCard)
     const d = new Date()
     const y = d.getFullYear()
     const m = (d.getMonth() + 1 + '').padStart(2, '0')
     const day = (d.getMonth() + 1 + '').padStart(2, '0')
-    console.log(d.getMonth() + 1)
     const age = parseInt((`${y}${m}${day}` - birthday) / 10000)
     return {
-      ...this.getBirthday(),
+      ...this.getBirthday(idCard),
       age
     }
   }
@@ -128,8 +129,8 @@ class IdCardFormat {
    * 查询性别
    * @returns {Object}
    */
-  getGender() {
-    const code = this.getSpecifiedCode([-2, 18])
+  getGender(idCard) {
+    const code = this.#getSpecifiedCode([-2, 17], idCard)
     // 奇数为男 偶数为女
     const isOdd = code % 2 === 1
     // 0 男 1 女
@@ -152,12 +153,17 @@ class IdCardFormat {
     * 获取身份证所有信息
     * @returns {Object}
    */
-  getIdCardInfo() {
+  getIdCardInfo(idCard) {
     return {
-      ...this.getCounty(),
-      ...this.getAge(),
-      ...this.getGender()
+      ...this.getCounty(idCard),
+      ...this.getAge(idCard),
+      ...this.getGender(idCard)
     }
+  }
+
+  idCardValidator(idCardNo) {
+    const reg = /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
+    return reg.test(idCardNo)
   }
 }
 
